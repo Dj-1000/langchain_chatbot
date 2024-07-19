e512319bb4fb980d1eead79c47e9bafa0f19c6b7
Dheeraj Sharma <dheeraj@snakescript.com>
2024-07-19 19:33:46 +0530

    updated chat models

--- chat/models.py
+++ chat/models.py
@@ -5,9 +5,9 @@ User = get_user_model()
 
 
 class Folder(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=2000)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subfolders')
-    description = models.CharField(max_length=500,blank=True)
+    description = models.CharField(max_length=2000,blank=True)
 
     def __str__(self) -> str:
         if self.parent:
@@ -18,7 +18,7 @@ class Folder(models.Model):
 
 class Room(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True)
-    name = models.CharField(max_length=200,default=None)
+    name = models.CharField(max_length=2000,default=None)
     member = models.ManyToManyField(User, related_name="rooms")
     category = models.ForeignKey(to=Folder, on_delete=models.CASCADE, related_name='sessions', null=True, default=None)
     is_matched = models.BooleanField(default=False)
@@ -44,7 +44,7 @@ class Room(models.Model):
 
 class Messages(models.Model):
     room = models.ForeignKey(Room, related_name='messages',on_delete=models.CASCADE,null=True)
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
     is_bot = models.BooleanField(default=False)
     sent_by = models.ForeignKey(User, related_name="messages_sent", on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -54,8 +54,8 @@ class Messages(models.Model):
 
 
 class File(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=500,blank=True)
+    name = models.CharField(max_length=2000)
+    description = models.CharField(max_length=2000,blank=True)
     folder = models.ForeignKey(Folder, related_name='files', on_delete=models.CASCADE,null=True,blank=True)
     file = models.FileField(upload_to='uploads/')
     created_at = models.DateTimeField(auto_now_add=True)
